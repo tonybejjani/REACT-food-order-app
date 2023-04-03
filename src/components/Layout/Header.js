e2b53cb201a29f4855e2a914144918454fab60b3
Tony <bejjani.tony@gmail.com>
2023-04-03 14:57:44 +1000

    1- create components folder and 4 subfolders 	Cart 	Layout 	Meals 	UI 2- Inside Layout component 	create Header component and Header.module.css 	add a folder assets with meals image 	incorporate image in file 	add css module classes
    3- integrate Header component in App.js

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,21 @@
+/** @format */
+
+import { React, Fragment } from 'react';
+
+import mealsImage from '../../assets/meals.jpg';
+import classes from './Header.module.css';
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>Catering Services</h1>
+        <button>Cart</button>
+      </header>
+      <div className={classes['main-image']}>
+        <img src={mealsImage} alt="a table full of kept warm meals" />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
