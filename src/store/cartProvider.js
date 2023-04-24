c60d0acd24cf737f2e676439980c63403fc854c6
Tony <bejjani.tony@gmail.com>
2023-04-24 14:29:13 +1000

    adjust cartContext which was initially wrong as the naminng convention given to is the same as the initial state of the useReducer function

--- src/store/cartProvider.js
+++ src/store/cartProvider.js
@@ -3,6 +3,99 @@
 import { useReducer } from 'react';
 import CartContext from './cart-context';
 
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
+
+const ACTIONS = {
+  ADD_CART_ITEM: 'add-cart-item',
+  REMOVE_CART_ITEM: 'remove-cart-item',
+};
+
+const reduce = (state, action) => {
+  let updatedItem;
+  let updatedItems = [...state.items];
+  //retrieve index of the item already available in updated Index
+
+  switch (action.type) {
+    case ACTIONS.ADD_CART_ITEM:
+      // locate index of item and update amount
+      // else just add the item to array of updated items
+
+      const itemIndex = updatedItems.findIndex((item) => {
+        return item.id === action.item.id;
+      });
+      let existingItem = updatedItems[itemIndex];
+
+      if (existingItem) {
+        updatedItem = {
+          ...existingItem,
+          amount: existingItem.amount + action.item.amount,
+        };
+
+        updatedItems[itemIndex] = updatedItem;
+      } else {
+        updatedItems = [...state.items, action.item];
+      }
+
+      const updatedTotalAmount =
+        state.totalAmount + action.item.price * action.item.amount;
+
+      return {
+        items: updatedItems,
+        totalAmount: updatedTotalAmount,
+      };
+
+    case ACTIONS.REMOVE_CART_ITEM:
+      // get item to decrement index in items array
+      const itemToDecrementIndex = updatedItems.findIndex((item) => {
+        return item.id === action.id;
+      });
+
+      let itemToDecrement = updatedItems[itemToDecrementIndex];
+
+      // on decrement, update total amount
+      let remainingTotalAmount = Math.abs(
+        state.totalAmount - itemToDecrement.price
+      );
+
+      let remainingItems;
+
+      // get  index of item to remove and decrement amount
+      const itemToDecrementAmount = itemToDecrement.amount - 1;
+
+      // if amount left is 0, remove item from Cart
+      if (itemToDecrementAmount === 0) {
+        remainingItems = updatedItems.filter((item) => {
+          return item.id !== action.id;
+        });
+
+        // if amount left > 0, decrement amount
+      } else {
+        updatedItems[itemToDecrementIndex].amount =
+          updatedItems[itemToDecrementIndex].amount - 1;
+        remainingItems = [...updatedItems];
+
+        // another way
+        // remainingItems = updatedItems.map((item) => {
+        //   if (item.id === action.id) {
+        //     item.amount = item.amount - 1;
+        //     return item;
+        //   }
+        //   return item;
+      }
+
+      return {
+        items: remainingItems,
+        totalAmount: remainingTotalAmount,
+      };
+
+    default:
+      return defaultCartState;
+  }
+};
+
 const CartProvider = (props) => {
   const addItemToCartHandler = (item) => {
     dispatch({ type: ACTIONS.ADD_CART_ITEM, item });
@@ -12,99 +105,14 @@ const CartProvider = (props) => {
     dispatch({ type: ACTIONS.REMOVE_CART_ITEM, id });
   };
 
-  const ACTIONS = {
-    ADD_CART_ITEM: 'add-cart-item',
-    REMOVE_CART_ITEM: 'remove-cart-item',
-  };
+  const [cartState, dispatch] = useReducer(reduce, defaultCartState);
 
-  const reduce = (state, action) => {
-    let updatedItem;
-    let updatedItems = [...state.items];
-    //retrieve index of the item already available in updated Index
-
-    switch (action.type) {
-      case ACTIONS.ADD_CART_ITEM:
-        // locate index of item and update amount
-        // else just add the item to array of updated items
-
-        const itemIndex = updatedItems.findIndex((item) => {
-          return item.id === action.item.id;
-        });
-        let existingItem = updatedItems[itemIndex];
-
-        if (existingItem) {
-          updatedItem = {
-            ...existingItem,
-            amount: existingItem.amount + action.item.amount,
-          };
-
-          updatedItems[itemIndex] = updatedItem;
-        } else {
-          updatedItems = [...state.items, action.item];
-        }
-
-        const updatedTotalAmount =
-          state.totalAmount + action.item.price * action.item.amount;
-
-        return {
-          ...state,
-          items: updatedItems,
-          totalAmount: updatedTotalAmount,
-        };
-
-      case ACTIONS.REMOVE_CART_ITEM:
-        // get item to decrement index in items array
-        const itemToDecrementIndex = updatedItems.findIndex((item) => {
-          return item.id === action.id;
-        });
-
-        let itemToDecrement = updatedItems[itemToDecrementIndex];
-
-        // on decrement, update total amount
-        let remainingTotalAmount = Math.abs(
-          state.totalAmount - itemToDecrement.price
-        );
-
-        let remainingItems;
-
-        // get  index of item to remove and decrement amount
-        const itemToDecrementAmount = itemToDecrement.amount - 1;
-
-        // if amount left is 0, remove item from Cart
-        if (itemToDecrementAmount === 0) {
-          remainingItems = updatedItems.filter((item) => {
-            return item.id !== action.id;
-          });
-
-          // if amount left > 0, decrement amount
-        } else {
-          remainingItems = updatedItems.map((item) => {
-            if (item.id === action.id) {
-              item.amount = item.amount - 1;
-              return item;
-            }
-            return item;
-          });
-        }
-
-        return {
-          ...state,
-          items: remainingItems,
-          totalAmount: remainingTotalAmount,
-        };
-
-      default:
-        return state;
-    }
-  };
-
-  const [cartContext, dispatch] = useReducer(reduce, {
-    items: [],
+  const cartContext = {
+    items: cartState.items,
     totalAmount: 0,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
-  });
-
+  };
   return (
     <CartContext.Provider value={cartContext}>
       {props.children}
